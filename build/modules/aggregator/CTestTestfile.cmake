1d28b0ce58bfc96f556ba5e333e62a9c1c52bf9f
SolomichevAnatoliy <system.unite.v1@gmail.com>
2026-04-26 19:30:28 +0000

    Remove build

--- build/modules/aggregator/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/modules/aggregator
-# Build directory: /workspace/build/modules/aggregator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(aggregator_tests "/workspace/build/modules/aggregator/aggregator_tests")
-set_tests_properties(aggregator_tests PROPERTIES  _BACKTRACE_TRIPLES "/workspace/modules/aggregator/CMakeLists.txt;52;add_test;/workspace/modules/aggregator/CMakeLists.txt;0;")
